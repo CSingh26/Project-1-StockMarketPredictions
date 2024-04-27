c23e771d617756b34de453dfc9ea36c4fb6c41bd
Csingh26 <singh.chaittanya@gmail.com>
2024-04-27 10:34:47 +0530

    rsi

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ for ax, stock in zip(axes.flatten(), (data['ticker'].unique()[:4])):
 plt.tight_layout()
 plt.show()
 
+#Avg Stock Prices
 avgStockPrice = {}
 
 for stock in data['ticker'].unique():
@@ -48,6 +49,7 @@ for stock in data['ticker'].unique():
 
 numericAvgStockPrice = {key: value for key, value in avgStockPrice.items() if isinstance(value, (int, float))}
 
+plt.figure(figsize=(100,10))
 plt.bar(range(len(numericAvgStockPrice)), numericAvgStockPrice.values(), align='center')
 plt.xlabel('Stock Names')
 plt.ylabel('Avg Price')
@@ -59,4 +61,40 @@ plt.title('Avg Stock Prices')
 for i, v in enumerate(numericAvgStockPrice.values()):
     plt.text(i, v, f'{v:.2f}', ha='center', va='bottom')
 
+plt.show()
+
+# Realtive Strength Index (RSI) for first 4 stocks
+def rsi(data, window=14):
+    delta = data['close'].diff()
+
+    gain = delta.where(delta > 0.0)
+    loss = -delta.where(delta < 0.0)
+
+    avgGain = gain.rolling(window=window, min_periods=1).mean()
+    avgLoss = loss.rolling(window=window, min_periods=1).mean()
+
+    rsi = 100 - (100 / (1 + (avgGain / avgLoss)))
+
+    return rsi
+
+def plotRSI(data, ticker, ax):
+    data['date'] = pd.to_datetime(data['date'])
+    ax.plot(data['date'], data['RSI'], label='RSI', color='orange')
+    ax.set_title(f'Relative Strength Index (RSI) for {ticker}')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('RSI')
+    ax.legend()
+    ax.grid(True)
+
+    ax.xaxis.set_major_locator(mdates.MonthLocator())
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%B'))
+
+fig, axes = plt.subplots(2, 2, figsize=(100,10))
+
+for ax, stock in zip(axes.flatten(), (data['ticker'].unique()[:4])):
+    stockName = data[data['ticker'] == stock].copy() 
+    stockName['RSI'] = rsi(stockName)
+    plotRSI(stockName, stock, ax)
+
+plt.tight_layout()
 plt.show()
